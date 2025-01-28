4502fb3426fefcfa917d9355c05c66937e8a3176
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2025-01-28 12:00:00 +0530

    HeroSection Done.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,28 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-scroll'
+import {GiHamburgerMenu} from 'react-icons/gi'
 
 const Navbar = () => {
+  const [show, setShow] = useState(false)
   return (
     <>
-        <h1>Hello World!!!...</h1>
+      <nav>
+        <div className='logo'>Event</div>
+        <div className={show ? "navLinks showMenu" : "navLinks"}>
+          <div className="links">
+            <Link to='landing' spy={true} smooth={true} duration={500} >HOME</Link>
+            <Link to='services' spy={true} smooth={true} duration={500} >SERVICES</Link>
+            <Link to='about' spy={true} smooth={true} duration={500} >ABOUT</Link>
+            <Link to='contact' spy={true} smooth={true} duration={500} >CONTACT</Link>
+          </div>
+        </div>
+        <div className='hamburger' onClick={() => {
+          setShow(!show)
+        }}>
+          <GiHamburgerMenu />
+        </div>
+      </nav>
     </>
   )
 }
