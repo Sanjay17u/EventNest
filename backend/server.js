35bacb5234549bcb0b243d39f72268f0cd0d74f7
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2025-01-28 17:34:16 +0530

    Application Ready For Deployment.

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,6 @@ import app from "./app.js";
 
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on ${port}`)
 })
