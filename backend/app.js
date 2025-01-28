29a5a726dca7102750d44ceb8fc9adcc586df800
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2025-01-28 07:55:09 +0530

    Backend Done and FrontEnd Setup

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,14 @@ import express from 'express'
 import { dbConnection } from './database/dbConnection.js'
 import dotenv from 'dotenv'
 import messageRouter from './router/message.router.js'
+import cors from 'cors'
 dotenv.config()
+
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["POST"],
+    credentials: true
+}))
 
 const app = express()
 
